7935421a9fa1f8e88e598171a9e873d4b3d062f5
Sevak Grigorian <sevak.grigoryan2021@gmail.com>
2023-03-08 14:02:36 +0400

    first commit

---
+++ src/utils/constants.jsx
@@ -0,0 +1,32 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon /> },
+  { name: 'Michael Jackson', icon: <MusicNoteIcon /> },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Podcast', icon: <GraphicEqIcon /> },
+  { name: 'Movie', icon: <OndemandVideoIcon /> },
+  { name: 'Comedy', icon: <TheaterComedyIcon /> },
+  { name: 'Sport', icon: <FitnessCenterIcon /> },
+  { name: 'Fashion', icon: <CheckroomIcon /> },
+  { name: 'Live', icon: <LiveTvIcon /> },
+];
+
+export const demoThumbnailUrl =
+  'https://img.youtube.com/vi/Zi_XLOBDo_Y/sddefault.jpg';
+export const demoChannelUrl = '/channel/UC5OrDvL9DscpcAstz7JnQGA';
+export const demoVideoUrl =
+  '/video/Zi_XLOBDo_Y';
+export const demoChannelTitle = 'Michael Jackson';
+export const demoVideoTitle = 'Michael Jackson - Billie Jean (Official Video)';
+export const demoProfilePicture =
+  'http://dergipark.org.tr/assets/app/images/buddy_sample.png';
